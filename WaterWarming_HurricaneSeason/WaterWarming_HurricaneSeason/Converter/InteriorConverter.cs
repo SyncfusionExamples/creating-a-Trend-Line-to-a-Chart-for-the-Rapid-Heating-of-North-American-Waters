55a7595923b0f1033dd1e4f98037137d0dc8384e
NanthiniMahalingam <nanthini.mahalingam@syncfusion.com>
2023-08-24 14:51:11 +0530

    Commit the sample and update read me file.

---
+++ WaterWarming_HurricaneSeason/WaterWarming_HurricaneSeason/Converter/InteriorConverter.cs
@@ -0,0 +1,79 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WaterWarming_HurricaneSeason
+{
+    public class InteriorConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as ChartSegment;
+
+            if (segment != null)
+            {
+                if ((segment.Item as YearlyTemperatureAnomaly).Temperature > 0)
+                {
+
+                    // green 47, 247, 2
+                    // dark green - 44, 219, 4
+                    // blue 82, 233, 250
+                    return new SolidColorBrush(Color.FromRgb(44, 219, 4));
+                }
+                else
+                {
+                    //Dark pink- 2, 218, 247
+                    //230, 7, 159
+                    return new SolidColorBrush(Color.FromRgb(230, 7, 159));
+                }
+
+            }
+
+            return new SolidColorBrush(Colors.Gray);
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (object)value;
+        }
+    }
+
+    public class TrackBallInterior : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var segment = value as ChartPointInfo;
+
+            if (segment != null && segment.Item != null)
+            {
+                if ((segment.Item as YearlyTemperatureAnomaly).Temperature > 0)
+                {
+                    // green 47, 247, 2
+                    //dark green
+                    // blue 82, 233, 250
+                    return new SolidColorBrush(Color.FromRgb(44, 219, 4));
+                }
+                else
+                {
+                    //Dark pink- 2, 218, 247
+                    //230, 7, 159
+                    return new SolidColorBrush(Color.FromRgb(242, 61, 184));
+                }
+
+            }
+
+            return new SolidColorBrush(Colors.Gray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WaterWarming_HurricaneSeason/WaterWarming_HurricaneSeason/Model/YearlyTemperatureAnamoly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterWarming_HurricaneSeason
+{
+    public class YearlyTemperatureAnomaly
+    {
+        public DateTime Year { get; set; }
+        public double Temperature { get; set; }
+
+        public YearlyTemperatureAnomaly(DateTime year,double temperature) 
+        {
+            Year = year;
+            Temperature = temperature;
+        }
+    }
+}
